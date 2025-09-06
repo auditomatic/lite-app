2b906802575e59415701b87b70e07423b6af5cdf
staeiou <sgeiger@gmail.com>
2025-09-06 17:55:45 +0000

    Deploy main: 37d7056e5a222bd74831af1e90659fc5ca6c3c6b

---
+++ assets/id-generator-Cv_Imc0S.js
@@ -0,0 +1,1 @@
+function r(t){const n=Date.now().toString(36),o=Math.random().toString(36).substr(2,9);return`${t}-${n}-${o}`}export{r as g};
---
+++ assets/index-Brb0k-N6.js
@@ -0,0 +1,1 @@
+function a(r){return r.type==="playground"||r.id.startsWith("playground-")}function n(r){return r.startsWith("playground-")}export{n as a,a as i};
