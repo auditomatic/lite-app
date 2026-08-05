8667281d935047cf42d2ae1b2111f1ce4b2fb966
staeiou <sgeiger@gmail.com>
2026-08-05 19:08:34 +0000

    Deploy main: d434f6d44c18932a27af34aa6ff9a861ba0c2a00

---
+++ assets/timestamps-CGloZApi.js
@@ -0,0 +1,2 @@
+function r(t){return t?new Date(t).toLocaleDateString():""}function e(t){return t?new Date(t).toLocaleString():"unknown"}function a(t,n){return`Updated: ${e(n)}
+Created: ${e(t)}`}export{a,r as f};
