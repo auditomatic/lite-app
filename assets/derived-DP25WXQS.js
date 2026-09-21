a4c1d13e7e29f980e2df73c59fc9efc834b79ebc
staeiou <sgeiger@gmail.com>
2026-09-21 19:36:11 +0000

    Deploy main: 43abea5036723dcec26e71095e590e2b7c96be89

---
+++ assets/derived-DP25WXQS.js
@@ -0,0 +1,1 @@
+import{D as c,E as e,F as a,G as p}from"./index-Bpe7iBPn.js";function u(r,o){const t=c(r),n=e(r),s=o??a(r);return{contract:t?.contract??null,errors:n,prose:t?.contract?p(t.contract,s):"",context:t}}export{u as d};
