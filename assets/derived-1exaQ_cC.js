7a288e30ab1309552074802edf575d1f179ea1f1
staeiou <sgeiger@gmail.com>
2026-09-22 17:36:35 +0000

    Deploy main: a9e801fdb9475edeef70f0a7bbb853529832ce71

---
+++ assets/derived-1exaQ_cC.js
@@ -0,0 +1,1 @@
+import{W as c,X as e,S as a,R as p}from"./index-Ctr3VH5Q.js";function u(r,o){const t=c(r),n=e(r),s=o??a(r);return{contract:t?.contract??null,errors:n,prose:t?.contract?p(t.contract,s):"",context:t}}export{u as d};
